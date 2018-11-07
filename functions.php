fec1dc6083bc382f467e843aeba6739670af9308
Paul Oranje <por@oranjevos.nl>
2018-11-07 17:13:54 +0100

    in footer onepress link vervangen door vimexx affiliate link

--- functions.php
+++ functions.php
@@ -11,29 +11,17 @@ add_action( 'wp_enqueue_scripts', 'onepress_child_enqueue_styles', 15 );
 function onepress_child_enqueue_styles() {
     wp_enqueue_style( 'onepress-child-style', get_stylesheet_directory_uri() . '/style.css' );
 }
-/**
- * Hook to add custom section after about section
- *
- * @see wp-content/themes/onepress/template-frontpage.php
- */
 
-/*
-function add_my_custom_section(){
-    ?>
-    <section id="my_section" class="my_section section-padding onepage-section">
-        <div class="container">
-            <div class="section-title-area">
-                <h5 class="section-subtitle"> My section subtitle</h5>
-                <h2 class="section-title"> My section title</h2>
-            </div>
-            <div class="row">
-                <!-- Your section content here, you can use bootstrap 4 elements :) -->
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
-
-            </div>
-        </div>
-    </section>
-    <?php
+add_filter('wp_head', 'custom_onepress_site_info', 999);
+function custom_onepress_site_info() {
+    remove_action( 'onepress_footer_site_info', 'onepress_footer_site_info' );
+    add_action( 'onepress_footer_site_info', 'custom_onepress_footer_site_info' );
 }
-add_action( 'onepress_after_section_about', 'add_my_custom_section'  );
-*/
+
+function custom_onepress_footer_site_info() {
+?>
+    <?php printf(esc_html__('Copyright %1$s %2$s %3$s', 'onepress'), '&copy;', esc_attr(date('Y')), esc_attr(get_bloginfo())); ?>
+    <span class="sep"> &ndash; </span>
+    <?php printf(esc_html__('Webhosting door %1$s', 'onepress'), '<a href="' . esc_url('https://www.vimexx.nl?affiliate=tcbths1jy9uiowaheglk') . '">Vimexx</a>'); ?>
+<?php
+}
